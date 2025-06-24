9c73183ceddcde87bbf057ca576434df90eef5b5
konradochedzan <konrad.ochedzan@gmail.com>
2025-06-24 12:57:34 +0200

    usage of saving

--- final_model_training.py
+++ final_model_training.py
@@ -7,6 +7,49 @@ from Simple_models import SimpleConvolutional, SimpleFeedForward, SimpleLSTM, Si
 
 from saving import save_model, load_model, save_predictions, load_predictions
 
+all_models_with_kwargs = {
+    'temporalconvnet': [TemporalConvNet, {
+        'num_channels': [32, 64, 32],
+        'kernel_size': 5,
+        'dropout': 0.1
+    }]
+    , 'cnn': [SimpleConvolutional, {
+        'num_channels': [32, 64, 32],
+        'kernel_size': 5,
+        'dropout': 0.1
+    }]
+    , 'feedforward': [SimpleFeedForward, {
+        'hidden_dims': 300,
+        'dropout': 0.1}],
+    'lstm': [SimpleLSTM, {
+        'hidden_dims': 200,
+        'dropout': 0.1,
+        'num_layers': 3}],
+    'transformer': [SimpleTransformer, {
+        'num_heads': 8,
+        'hidden_dims': 256,
+        'dropout': 0.1,
+        'num_layers': 3}],
+    'temporalfusiontransformer': [TemporalFusionTransformer, {
+        'hidden_dims': 128,
+        'num_heads': 4,
+        'dropout': 0.1,
+        'num_layers': 2}],}
+    
+FIXED_PARAMS = {
+    'window_strategy': 'rolling',
+    'train_window_years': 3,
+    'test_window_years': 1,
+    'use_autoencoder': True,
+    'encoding_dim': 10,
+    'seq_length': 24,
+    'epochs': 120,
+    'lr': 0.0001,
+    'batch_size': 128,
+    'device': 'cpu',
+    'plot_results': False,
+    'do_print': False
+}
 
 df = pd.read_csv('data_non_std.csv', parse_dates=['Unnamed: 0'])
 df.rename(columns={'Unnamed: 0': 'Date'}, inplace=True)
@@ -15,27 +58,23 @@ target = df['returns'].values.astype(np.float32)
 dates = pd.to_datetime(df['Date'])
 tbill3m = df['tbill3m'].values.astype(np.float32)
 
-results_tcn = sp500_training_pipeline(
-    X=features,
-    y=target,
-    dates=dates,
-    tbill3m=tbill3m_data,
-   model_class  = TemporalConvNet,
-    model_type   = 'temporalconvnet',                    # or 'simpleconvolutional'
-    model_kwargs = {
-        'num_channels': [32, 64,128,64, 32], 'kernel_size': 5, 'dropout': 0.1
-    },
-    window_strategy='rolling',
-    train_window_years=3,
-    test_window_years=1,
-    use_autoencoder=True,
-    encoding_dim=10,
-    seq_length=24,
-    ar_lags=1,
-    batch_size=128,
-    epochs=10,
-    lr=0.001,
-    plot_results=True,
-    alpha=0.0,
-    l1_ratio=0.0,
-)
+for model_type, (model_class, model_kwargs) in all_models_with_kwargs.items():
+    print(f"Training {model_type} model...")
+    results = sp500_training_pipeline(
+        X            = features,
+        y            = target,
+        dates        = dates,
+        tbill3m      = tbill3m,
+        model_class  = model_class,
+        model_type   = model_type,
+        model_kwargs = model_kwargs,
+        **FIXED_PARAMS,
+    )
+    save_model(results['model'], f'models/{model_type}_final_model.pkl')
+    save_predictions(
+        dates        = results["all_test_dates"],
+        predictions  = results["all_test_predictions"],
+        csv_path     = f"results/{model_type}_predictions.csv",
+    )
+    print(f"{model_type} model training complete. Results saved.")
+
